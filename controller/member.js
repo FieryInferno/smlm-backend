d5363d13a388e381a56093f32c0810190189e8da
FieryInferno <bagassetia271@gmail.com>
2022-10-10 22:06:32 +0700

    Add member

--- controller/member.js
+++ controller/member.js
@@ -4,13 +4,52 @@ let status;
 let message;
 
 const {Member} = require('../models');
+const {validationResult} = require('express-validator');
+const {Op} = require('sequelize');
 
 const getAll = async (req, res) => {
   try {
-    data = await Member.findAndCountAll({include: ['parent', 'children']});
+    const {limit, filter} = req.query;
+
+    data = await Member.findAndCountAll({
+      include: ['parent', 'children'],
+      limit,
+      where: filter ? {member: {[Op.iLike]: `%${filter}%`}} : '',
+    });
+
     status = 'Success';
     message = 'Success';
     code = 200;
+  } catch (error) {
+    console.log(error);
+    data = {};
+    status = 'Failed';
+    message = error;
+    code = 400;
+  } finally {
+    return res.status(code).send({
+      status,
+      message,
+      data,
+    });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      status = 'Bad Request';
+      message = errors;
+      code = 400;
+      data = {};
+    } else {
+      data = await Member.create(req.body);
+      status = 'Success';
+      message = 'Success';
+      code = 201;
+    }
   } catch (error) {
     data = {};
     status = 'Failed';
@@ -25,4 +64,4 @@ const getAll = async (req, res) => {
   }
 };
 
-module.exports = {getAll};
+module.exports = {getAll, create};
--- routes/member.js
+++ routes/member.js
@@ -1,4 +1,5 @@
 const controller = require('../controller/member');
+const {body} = require('express-validator');
 
 module.exports = (app) => {
   app.use((req, res, next) => {
@@ -11,4 +12,9 @@ module.exports = (app) => {
   });
 
   app.get('/member', controller.getAll);
+  app.post(
+      '/member',
+      body('member').notEmpty().withMessage('Member cannot be null'),
+      controller.create,
+  );
 };
