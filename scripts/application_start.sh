3a01e5e4e11c4dd6d306bbdfdda5f0438870c89b
FieryInferno <bagassetia271@gmail.com>
2022-11-09 16:52:41 +0700

    fix: Application start and timeout

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+#give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ubuntu/smlm-backend
+#navigate into our working directory where we have all our github files
 cd /home/ubuntu/smlm-backend
+#add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in path now)
+#install node modules
 npm install
+#start our node app in the background
 node index.js
