df6f698362b3c91e34ce8b20e191069121e8efa4
FieryInferno <bagassetia271@gmail.com>
2022-11-09 16:45:34 +0700

    fix: Application start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo chmod -R 777 /home/ubuntu/smlm-backend
 cd /home/ubuntu/smlm-backend
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 npm install
 node index.js
